ecb2731a87dd77c376c210717cc6f3e3f40da462
Mano <manorajsivanmalai>
2025-03-29 14:27:03 +0530

    all done production level

--- server.js
+++ server.js
@@ -28,3 +28,5 @@ app.use("/api/order", orderRoutes);
 
 // const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
+module.exports.handler = serverless(app); 
